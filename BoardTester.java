be68636957ee8115de1e715d27944657a3cd3757
Skrumpcious <iamfes007@aol.com>
2018-04-10 22:18:11 -0700

    Base class and Board object created
    Created in order to be able to begin testing.

--- BoardTester.java
+++ BoardTester.java
@@ -1,1 +1,14 @@
 
+/**
+ *
+ */
+public class BoardTester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Board myBoard = new Board();
+    }
+    
+}
